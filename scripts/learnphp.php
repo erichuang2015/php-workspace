0a88293ae8ff4d57e407384160e21b3f364857f4
Johnathan R. Burgess <ryanburgess173@outlook.com>
2019-08-27 23:05:24 -0400

    added time signature output

--- scripts/learnphp.php
+++ scripts/learnphp.php
@@ -5,6 +5,8 @@
 $username = "Ryan Burgess";
 $htmlTagString = "<p>".$username."</p>";
 echo $htmlTagString;
+date_default_timezone_set('UTC');
+echo date('h:i:s:u a, l F jS Y e');
 ?>
 </body>
 </html>
