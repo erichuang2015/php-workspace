ccfb6d706af13892ba89f94fc42e8ca5e0833e38
Johnathan R. Burgess <ryanburgess173@outlook.com>
2019-08-27 23:11:34 -0400

    added output of user input passed using apache to php

--- scripts/learnphp.php
+++ scripts/learnphp.php
@@ -2,11 +2,22 @@
 
 <body>
 <?php
+/*
 $username = "Ryan Burgess";
 $htmlTagString = "<p>".$username."</p>";
 echo $htmlTagString;
 date_default_timezone_set('UTC');
-echo date('h:i:s:u a, l F jS Y e');
+echo date('h:i:s:u a, l F jS Y e');*/
+
+$userName = $_POST['username'];
+$address = $_POST['address'];
+$city = $_POST['city'];
+
+echo "<hr>";
+echo $userName . "<hr>";
+echo $address . "<hr>";
+echo $city . "<hr>";
+
 ?>
 </body>
 </html>
